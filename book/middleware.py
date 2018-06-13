22c686bfe0ca87b02cae326d7f023e5803b005a3
DESKTOP-C6A0IIE\Gui <gui.yun@e-ports.com>
2018-06-13 09:27:18 +0800

    request.META.get(REMOTE_ADDR), request.user.is_authenticated

--- book/middleware.py
+++ book/middleware.py
@@ -23,6 +23,7 @@ class BookMiddleware(object):
         print('*' * 64)
         user = request.user
         print("BookMiddleware executed by {}".format(user))
+        print(request.META.get('REMOTE_ADDR'), request.user.is_authenticated)
         if str(
                 user) == 'gui' and '/admin/' not in request.path:  # 如果是管理员gui并且当前路径不是/admin/跳转到/admin/，不加requst.path 会循环重定向
             print(request.user, request.path)
